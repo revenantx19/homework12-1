32f34d6e470b10ccf10856473cf68e8f65034dfa
Konstantin <reverant_01@mail.ru>
2024-02-11 19:39:42 +0300

    main

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Person sarah = new Person();
+
+        sarah.name = "Sarah";
+        System.out.println("Имя - " + sarah.name);
+
+        sarah.age = 30;
+        System.out.println("Возраст - " + sarah.age);
+    }
+}
+
